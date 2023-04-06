b32c26384df3340832a3f434ec8096e611240914
sakshiNimap <sakshidhenge@nimapinfotech.com>
2023-04-06 16:16:21 +0530

    fibonaci without recursion

---
+++ fibonaci-without-recursion.js
@@ -0,0 +1,17 @@
+// WAP to print Fibonacci series without recursion.
+
+function fibonacci(n) {
+    if (n === 0) return [];
+    if (n === 1) return [0];
+    if (n === 2) return [0, 1];
+    
+    let fib = [0, 1];
+    for (let i = 2; i < n; i++) {
+      fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    
+    return fib;
+  }
+  
+  
+  console.log(fibonacci(10)); // [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
